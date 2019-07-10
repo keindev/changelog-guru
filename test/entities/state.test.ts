7c11d90779ec95adcb8854d508301e0ec49af099
Daniil Ryazanov <kein@tagproject.ru>
2019-07-10 14:11:57 +0300

    test(State): add test for ignored entities !group(Tests)
    task: #jmctp
    
    Signed-off-by: Daniil Ryazanov <kein@tagproject.ru>

--- test/entities/state.test.ts
+++ test/entities/state.test.ts
@@ -6,42 +6,51 @@ import { Position } from '../../src/entities/section';
 
 const VERSION = '1.0.0';
 const config = new Config();
-const getAuthor = (index: number): Author =>
-    new Author(1001 + index, {
-        login: `keindev${index}`,
-        url: 'https://github.com/keindev',
+const getAuthor = (id: number, login: string): Author =>
+    new Author(id, {
+        login,
+        url: `https://github.com/${login}`,
         avatar: 'https://avatars3.githubusercontent.com/u/4527292?v=4',
     });
-const getCommit = (index: number): Commit =>
-    new Commit(`b816518030dace1b91838ae0abd56fa88eba19f${index}`, {
+const getCommit = (id: number, message: string, author: Author): Commit =>
+    new Commit(`b816518030dace1b91838ae0abd56fa88eba19f${id}`, {
+        message,
         timestamp: 0,
-        message: `feat(test): message`,
         url: 'https://github.com/keindev/changelog-guru/commit/b816518030dace1b91838ae0abd56fa88eba19f0',
-        author: 'keindev',
+        author: author.login,
     });
 
 describe('State', (): void => {
     it('Create', (done): void => {
-        const state = new State(VERSION);
-        const author1 = getAuthor(0);
-        const author2 = getAuthor(1);
-        const commit = getCommit(3);
-        const section = state.addSection('header section', Position.Header);
-        const emptySection = state.addSection('empty section', Position.Footer);
+        config.load().then((): void => {
+            const state = new State(VERSION);
+            const author1 = getAuthor(0, 'dev1');
+            const author2 = getAuthor(1, 'dev2');
+            const commit = getCommit(2, 'test(State): message3', author2);
+            const section = state.addSection('header section', Position.Header);
+            const emptySection = state.addSection('empty section', Position.Footer);
+            const ignoredAuthor = getAuthor(2, 'dependabot-preview[bot]');
+            const ignoredCommit = getCommit(3, 'build(deps): ignore this message', ignoredAuthor);
 
-        state.addCommit(getCommit(1), author1);
-        state.addCommit(getCommit(2), author1);
-        section.assign(commit);
-        state.addCommit(commit, author2);
+            state.addCommit(getCommit(0, 'test(State): message1', author1), author1);
+            state.addCommit(getCommit(1, 'test(State): message2', author1), author1);
+            section.assign(commit);
+            state.addCommit(commit, author2);
+            state.addCommit(ignoredCommit, ignoredAuthor);
 
-        expect(state.getAuthors()).toStrictEqual([author1, author2]);
-        expect(state.getSections()).toStrictEqual([section, emptySection]);
-        expect(state.getVersion()).toBe(VERSION);
+            expect(state.getAuthors()).toStrictEqual([author1, author2, ignoredAuthor]);
+            expect(state.getSections()).toStrictEqual([section, emptySection]);
+            expect(state.getVersion()).toBe(VERSION);
+            expect(ignoredAuthor.isIgnored()).toBeFalsy();
+            expect(ignoredCommit.isIgnored()).toBeFalsy();
 
-        state.modify(config).then((): void => {
-            expect(state.getSections()).toStrictEqual([section]);
+            state.modify(config).then((): void => {
+                expect(state.getSections()).toStrictEqual([section]);
+                expect(ignoredAuthor.isIgnored()).toBeTruthy();
+                expect(ignoredCommit.isIgnored()).toBeTruthy();
 
-            done();
+                done();
+            });
         });
     });
 
