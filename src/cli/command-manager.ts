f6a4b9276278ac9396134a522e6cadce15deabd2
Daniil Ryazanov <kein@tagproject.ru>
2019-08-16 01:09:45 +0300

    improve(cli): define display rules --help

--- src/cli/command-manager.ts
+++ src/cli/command-manager.ts
@@ -1,82 +1,105 @@
+import chalk from 'chalk';
 import commandLineArgs from 'command-line-args';
 import { TaskTree } from 'tasktree-cli';
 import { Command } from './commands/command';
-import { Help } from './help';
 
 export class CommandManager {
-    public static OPTION_SEPARATOR = ' ';
+    public static OPTION_NAME_MAX_LENGTH = 50;
+    public static LINE_SEPARATOR = '\n';
+    public static INDENT = ' ';
 
     private taskTree = TaskTree.tree();
-    private commands: Command[];
+    private commandName: string | undefined;
     private command: Command | undefined;
+    private commands: Command[];
+    private hasHelpOption: boolean;
     private argv: commandLineArgs.ParseOptions;
-    private unknownOptions: string[];
     private readonly helpDefinition: commandLineArgs.OptionDefinition = { name: 'help', type: Boolean };
 
     public constructor(commands: Command[]) {
-        const mainOptions = commandLineArgs([{ name: 'command', defaultOption: true }], { stopAtFirstUnknown: true });
+        const mainOptions = commandLineArgs([{ name: 'command', defaultOption: true }, this.helpDefinition], {
+            stopAtFirstUnknown: true,
+        });
 
         // eslint-disable-next-line no-underscore-dangle
-        this.unknownOptions = mainOptions._unknown || [];
-        this.argv = { argv: this.unknownOptions };
+        this.argv = { argv: mainOptions._unknown || [] };
+        this.hasHelpOption = mainOptions.help;
+        this.commandName = mainOptions.command;
+        this.command = commands.find((command): boolean => command.isMatched(mainOptions.command));
         this.commands = commands;
+    }
 
-        if (mainOptions.command) {
-            this.command = commands.find((command): boolean => command.isMatched(mainOptions.command));
+    private static print(lines: string | string[]): void {
+        const text = Array.isArray(lines) ? lines.join(CommandManager.INDENT) : lines;
 
-            if (!this.command) {
-                this.error(mainOptions.command);
-            } else if (!this.unknownOptions.length) {
-                // TODO: `changelog build` can be called without parameters
-                this.help(this.command);
-                this.command = undefined;
-            }
-        } else if (this.unknownOptions.length) {
-            this.error();
-        } else {
-            this.help();
-        }
+        process.stdout.write(`${text}${CommandManager.LINE_SEPARATOR}`);
+    }
+
+    private static renderCommandHelp(command: Command): string {
+        const name = command.alias ? `${command.alias}, ${command.name}` : command.name;
+        const options = command.hasOptions() ? chalk` {dim [options]}` : '';
+        const output = [
+            chalk`${CommandManager.LINE_SEPARATOR} {bold ${name}}${options} - ${command.description.toLowerCase()}`,
+        ];
+
+        command.getOptions().forEach(([option, description, type]): void => {
+            output.push(
+                `${chalk`    {bold --${option}} {dim ${type}}`.padEnd(
+                    CommandManager.OPTION_NAME_MAX_LENGTH,
+                    CommandManager.INDENT
+                )}${description}`
+            );
+        });
+
+        return output.join(CommandManager.LINE_SEPARATOR);
     }
 
     public async execute(): Promise<void> {
-        if (this.command) {
-            const command: Command = this.command as Command;
+        const { command, commandName } = this;
 
+        if (this.hasHelpOption || !commandName) {
+            this.printHelp();
+        } else if (!command) {
+            CommandManager.print([
+                chalk`changelog «{bold ${commandName}}» is not a changelog command.`,
+                chalk`See {bold «changelog --help»}.`,
+            ]);
+        } else {
             try {
-                const options = commandLineArgs([this.helpDefinition, ...command.getDefinitions()], this.argv);
+                const options = commandLineArgs(command.getDefinitions(), this.argv);
 
-                if (options.help) {
-                    this.help(command);
+                this.taskTree.start();
+                await command.execute(options);
+                this.taskTree.stop();
+            } catch (error) {
+                const { argv } = this.argv;
+
+                if (Array.isArray(argv) && argv.length) {
+                    CommandManager.print([
+                        chalk`command «{bold ${commandName}}» doesn't have option {dim ${argv.pop() as string}}.`,
+                        chalk`See {bold «changelog ${commandName} --help»}.`,
+                    ]);
                 } else {
-                    this.taskTree.start();
-
-                    await command.execute(options);
-
-                    this.taskTree.stop();
+                    throw error;
                 }
-            } catch {
-                this.error();
             }
         }
     }
 
-    private error(commandName: string = this.unknownOptions.join(CommandManager.OPTION_SEPARATOR)): void {
-        process.stdout.write(`${Help.unexpectedCommand(commandName)}${Help.LINE_SEPARATOR}`);
-    }
-
-    private help(command?: Command): void {
+    private printHelp(): void {
+        const { command } = this;
         let output: string;
 
         if (command) {
-            output = Help.command(command);
+            output = CommandManager.renderCommandHelp(command);
         } else {
             output = [
-                Help.header('Changelog guru:'),
-                Help.description('Git changelog generator, customizable a release changelog with helpful plugins'),
-                ...this.commands.map(Help.command),
-            ].join(Help.LINE_SEPARATOR);
+                chalk`{bold.underline Changelog guru:}`,
+                chalk` {dim Git changelog generator, customizable a release changelog with helpful plugins}`,
+                ...this.commands.map(CommandManager.renderCommandHelp),
+            ].join(CommandManager.LINE_SEPARATOR);
         }
 
-        process.stdout.write(`${output}${Help.LINE_SEPARATOR}`);
+        CommandManager.print(output);
     }
 }
