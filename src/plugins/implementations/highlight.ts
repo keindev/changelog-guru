67c365854b350d5ac15b0b10e54e18aa08083860
Alexei Delezhov <lexadel@tagproject.ru>
2019-08-21 20:44:48 +0300

    improve(Highlight): add ability checking camelCase expressions

--- src/plugins/implementations/highlight.ts
+++ src/plugins/implementations/highlight.ts
@@ -3,6 +3,7 @@ import { CommitPlugin } from '../commit-plugin';
 import { Commit } from '../../entities/commit';
 import { PluginOption } from '../../config/config';
 import Markdown from '../../utils/markdown';
+import { PluginLintOptions } from '../../linter';
 
 export enum MaskType {
     // Generics between tags
@@ -23,19 +24,26 @@ export default class HighlightPlugin extends CommitPlugin {
     private masks: string[] = [];
 
     public async init(config: HighlightPluginOptions): Promise<void> {
-        this.camelCase = config.camelCase || true;
+        this.camelCase = !!config.camelCase;
         this.masks = Object.values(MaskType);
         if (Array.isArray(config.masks) && config.masks.length > 0) this.masks.push(...config.masks);
     }
 
-    public async parse(commit: Commit, task: Task): Promise<void> {
+    public async parse(commit: Commit): Promise<void> {
         const subject = this.getSubjectFrom(commit);
         commit.setSubject(subject);
-        if (subject === '') task.complete('Empty subject');
     }
 
-    public lint(): void {
-        this.camelCase = this.camelCase || false;
+    public lint(options: PluginLintOptions, task: Task): void {
+        this.masks.forEach(mask => {
+            let isValid = true;
+            try {
+                RegExp(mask);
+            } catch (e) {
+                isValid = false;
+            }
+            if (!isValid) task.error(`Mask {bold ${mask}} is not valid`);
+        });
     }
 
     private getSubjectFrom(commit: Commit): string {
@@ -50,6 +58,13 @@ export default class HighlightPlugin extends CommitPlugin {
                     return Markdown.code(substring.trim());
                 });
             });
+            if (this.camelCase) {
+                const expression = new RegExp('[a-z]\\w*[A-Z]\\S*', 'g');
+                subject = subject.replace(expression, (substring): string => {
+                    escape = true;
+                    return Markdown.code(substring.trim());
+                });
+            }
         }
         if (escape) commit.escape();
         return subject;
