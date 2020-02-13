ff31f04ff61e498ae6e3dffdf365986377835c0f
Daniil Ryazanov <kein@tagproject.ru>
2020-02-14 02:30:17 +0300

    refactor: remove config, rename ConfigLoader to Config

--- src/core/config/ConfigLoader.ts
+++ src/core/config/ConfigLoader.ts
@@ -2,7 +2,25 @@ import path from 'path';
 import cosmiconfig from 'cosmiconfig';
 import deepmerge from 'deepmerge';
 import { TaskTree } from 'tasktree-cli';
-import Config, { ServiceProvider, ChangeLevel, ExclusionType } from './Config';
+import { IPluginConfig } from '../../plugins/Plugin';
+
+export enum ServiceProvider {
+    GitHub = 'github',
+    GitLab = 'gitlab',
+}
+
+export enum ChangeLevel {
+    Major = 'major',
+    Minor = 'minor',
+    Patch = 'patch',
+}
+
+export enum ExclusionType {
+    AuthorLogin = 'authorLogin',
+    CommitType = 'commitType',
+    CommitScope = 'commitScope',
+    CommitSubject = 'commitSubject',
+}
 
 export interface IConfigLoaderOptions {
     provider?: ServiceProvider;
@@ -11,13 +29,18 @@ export interface IConfigLoaderOptions {
     exclusions?: Map<ExclusionType, string[]>;
 }
 
-export default class ConfigLoader {
+export interface IConfig extends cosmiconfig.Config {
+    changes?: { [key in ChangeLevel]: string[] };
+    output?: { filePath?: string; exclude?: { [key in ExclusionType]: string[] } };
+    plugins?: { [key: string]: IPluginConfig };
+}
+
+export default class Config {
     static MODULE_NAME = 'changelog';
     static DEFAULT_CONFIG_PATH = `../../../.changelogrc.default.yml`;
     static DEFAULT_OUTPUT_FILE_NAME = 'CHANGELOG.md';
 
-    private filePath = path.join(__dirname, ConfigLoader.DEFAULT_CONFIG_PATH);
-    private data: cosmiconfig.Config = {};
+    private data: IConfig = {};
     private options: IConfigLoaderOptions;
 
     public constructor(options: IConfigLoaderOptions = {}) {
@@ -51,66 +74,61 @@ export default class ConfigLoader {
     private getTypes(): Map<string, ChangeLevel> {
         const types = new Map<string, ChangeLevel>();
         const { changes } = this.data;
+        const levels = Object.values(ChangeLevel);
 
         if (changes) {
-            const levels = Object.values<string>(ChangeLevel);
+            Object.values(ChangeLevel).reduce((acc, level) => {
+                if (Array.isArray(changes[level]))
+                    acc.push(...changes[level].map((name): [string, ChangeLevel] => [name, level]));
 
-            Object.entries(changes).reduce((acc, [level, names]): [string, ChangeLevel][] => {
-                if (levels.includes(level)) TaskTree.fail('Unexpected level of changes');
-
-                return Array.isArray(names) ? [...acc, ...names.map(name => [name, level])] : acc;
-            }, []);
-
-            Object.entries(changes).forEach(([level, names]) => {
-                if (Array.isArray(names)) {
-                    if (levels.includes(level)) TaskTree.fail('Unexpected level of changes');
-
-                    names.forEach(name => {
-                        types.set(name, level as ChangeLevel);
-                    });
-                }
-            });
+                return acc;
+            }, [] as [string, ChangeLevel][]);
         }
+
+        Object.entries(changes).forEach(([level, names]) => {
+            if (!Array.isArray(names)) TaskTree.fail(`Names of change level "${level}" must be array`);
+            if (!levels.includes(level)) TaskTree.fail(`Unexpected level "${level}" of changes`);
+
+            names.forEach(name => {
+                types.set(name, level as ChangeLevel);
+            });
+        });
 
         return types;
     }
 
     private getExclusions(): Map<ExclusionType, string[]> {
         const exclusions: Map<ExclusionType, string[]> = new Map();
-        const {
-            data: { output },
-        } = this;
+        const { output } = this.data;
 
-        if (output && output.exclude) {
-            Object.entries<string[]>(output.exclude).forEach(([name, rules]) => {
-                if (Object.values(ExclusionType).includes(name as ExclusionType)) {
-                    exclusions.set(name as ExclusionType, [...new Set(rules)]);
-                } else {
-                    TaskTree.fail('Unexpected exclusion name');
-                }
+        if (output?.exclude) {
+            const types = Object.values(ExclusionType);
+
+            Object.entries(output.exclude).forEach(([name, rules]) => {
+                if (!types.includes(name)) TaskTree.fail('Unexpected exclusion name');
+
+                exclusions.set(name, [...new Set(rules)]);
             });
         }
 
         return exclusions;
     }
 
-    private getPlugins(): Map<string, IPluginOption> {
-        const activePlugins: Map<string, IPluginOption> = new Map();
-        const {
-            data: { plugins },
-        } = this;
+    private getPlugins(): Map<string, IPluginConfig> {
+        const activePlugins: Map<string, IPluginConfig> = new Map();
+        const { plugins } = this.data;
 
         if (Array.isArray(plugins)) {
             plugins.forEach(plugin => {
                 if (typeof plugin === 'string') {
                     activePlugins.set(plugin, {});
                 } else {
-                    Object.entries<IPluginOption>(plugin).forEach(([name, config]) => {
+                    Object.entries<IPluginConfig>(plugin).forEach(([name, config]) => {
                         if (config) {
                             activePlugins.set(
                                 name,
                                 new Proxy(config, {
-                                    get(target, fieldName, receiver): IPluginOption {
+                                    get(target, fieldName, receiver): IPluginConfig {
                                         return Reflect.get(target, fieldName, receiver);
                                     },
                                     set(): boolean {
@@ -128,9 +146,16 @@ export default class ConfigLoader {
     }
 
     private async loadConfig(): Promise<string | undefined> {
-        const explorer = cosmiconfig(ConfigLoader.MODULE_NAME);
+        const explorer = cosmiconfig(Config.MODULE_NAME);
+        /*        const configs = await Promise.all([
+            explorer.load(path.join(__dirname, Config.DEFAULT_CONFIG_PATH)),
+            explorer.search(),
+        ]).then(items => items.filter(item => item && item.isEmpty));
+
+        this.data = configs[0] && configs.length === 2 ? deepmerge(configs[0]!, configs[1]!) : configs[0];
+*/
+        const defaultConfig = await explorer.load(path.join(__dirname, Config.DEFAULT_CONFIG_PATH));
         const externalConfig = await explorer.search();
-        const defaultConfig = await explorer.load(this.filePath);
         let filePath: string | undefined;
 
         if (defaultConfig && !defaultConfig.isEmpty) {
@@ -146,3 +171,49 @@ export default class ConfigLoader {
         return filePath;
     }
 }
+/*
+
+
+export interface IConfigOptions {
+    provider: ServiceProvider;
+    filePath: string;
+    types: Map<string, ChangeLevel>;
+    exclusions: Map<ExclusionType, string[]>;
+    plugins: Map<string, IPluginOption>;
+}
+
+export default class Config {
+    readonly filePath: string;
+    readonly provider: ServiceProvider;
+
+    // FIXME: rename after update to TS 3.8
+    private types: Map<string, ChangeLevel>;
+    private plugins: Map<string, IPluginOption>;
+    private exclusions: Map<ExclusionType, string[]>;
+
+    constructor(options: IConfigOptions) {
+        this.provider = options.provider;
+        this.filePath = options.filePath;
+        this.types = options.types;
+        this.plugins = options.plugins;
+        this.exclusions = options.exclusions;
+    }
+
+    get plugins(): [string, IPluginOption][] {
+        return [...this.plugins.entries()];
+    }
+
+    get types(): [string, ChangeLevel][] {
+        return [...this.types.entries()];
+    }
+
+    get exclusions(): [ExclusionType, string[]][] {
+        return [...this.exclusions.entries()];
+    }
+
+    public getPlugin(name: string): IPluginOption | undefined {
+        return this.plugins.get(name);
+    }
+}
+
+*/
