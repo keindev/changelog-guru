1d3641deb7d502d73401a5136f62d9fe023f495d
keindev <kein@tagproject.ru>
2021-05-07 00:32:06 +0300

    fix: add .js ext to bin script

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -7,12 +7,3 @@ import lint from './commands/lint';
 const argv = yargs(hideBin(process.argv));
 
 argv.command(generate).command(lint).demandCommand().wrap(argv.terminalWidth()).help().parse();
-
-/*
-argv
-  .commandDir('commands', { extensions: ['js', 'ts'], exclude: /.+\.d\.ts/ })
-  .demandCommand()
-  .wrap(argv.terminalWidth())
-  .help()
-  .parse();
-*/
